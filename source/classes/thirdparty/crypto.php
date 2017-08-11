c6a035b447b74b0a6bc65d21f118a9ce3783cbcc
Tomas Tornevall <tomas.tornevall@resurs.se>
2017-08-11 10:23:37 +0200

    CryptoLIB patch end reformatting

--- source/classes/thirdparty/crypto.php
+++ source/classes/thirdparty/crypto.php
@@ -4,14 +4,18 @@ namespace TorneLIB;
 
 /**
  * Class TorneLIB_Crypto Encryption and encoding
- * 
+ *
  * @package TorneLIB
  */
-class TorneLIB_Crypto {
-    
+class TorneLIB_Crypto
+{
+
     private $aesKey = "";
     private $aesIv = "";
-    
+
+    /**
+     * TorneLIB_Crypto constructor.
+     */
     function __construct()
     {
         $this->setAesIv(md5("TorneLIB Default IV - Please Change this"));
@@ -33,10 +37,7 @@ class TorneLIB_Crypto {
      * @param bool $webFriendly Set to true works best with the less complex strings as it only removes characters that could be mistaken by another character (O,0,1,l,I etc)
      * @return string
      */
-    /**
-     * @return mixed|null|string
-     */
-    function mkpass ($complexity = 4, $setMax = 8, $webFriendly = false)
+    function mkpass($complexity = 4, $setMax = 8, $webFriendly = false)
     {
         $returnString = null;
         $characterListArray = array(
@@ -46,30 +47,41 @@ class TorneLIB_Crypto {
             '!@#$%*?'
         );
         // Set complexity to no limit if type 6 is requested
-        if ($complexity == 6)
-        {
-            $characterListArray = array('0'=>'');
-            for ($unlim = 0; $unlim <= 255 ; $unlim++) {
+        if ($complexity == 6) {
+            $characterListArray = array('0' => '');
+            for ($unlim = 0; $unlim <= 255; $unlim++) {
                 $characterListArray[0] .= chr($unlim);
             }
-            if ($setMax == null) {$setMax = 15;}
+            if ($setMax == null) {
+                $setMax = 15;
+            }
         }
         // Backward-compatibility in the complexity will still give us captcha-capabilities for simpler users
         $max = 8;       // Longest complexity
-        if ($complexity == 1) {unset($characterListArray[1], $characterListArray[2], $characterListArray[3]);$max = 6;}
-        if ($complexity == 2) {unset($characterListArray[2], $characterListArray[3]); $max = 10;}
-        if ($complexity == 3) {unset($characterListArray[3]); $max = 10;}
-        if ($setMax > 0) {$max = $setMax;}
+        if ($complexity == 1) {
+            unset($characterListArray[1], $characterListArray[2], $characterListArray[3]);
+            $max = 6;
+        }
+        if ($complexity == 2) {
+            unset($characterListArray[2], $characterListArray[3]);
+            $max = 10;
+        }
+        if ($complexity == 3) {
+            unset($characterListArray[3]);
+            $max = 10;
+        }
+        if ($setMax > 0) {
+            $max = $setMax;
+        }
         $chars = array();
         $numchars = array();
         $equalityPart = ceil($max / count($characterListArray));
-        for ($i = 0 ; $i < $max ; $i++)
-        {
-            $charListId = rand(0, count($characterListArray)-1);
+        for ($i = 0; $i < $max; $i++) {
+            $charListId = rand(0, count($characterListArray) - 1);
             if (!isset($numchars[$charListId])) {
                 $numchars[$charListId] = 0;
             }
-            $numchars[$charListId] ++;
+            $numchars[$charListId]++;
             $chars[] = $characterListArray[$charListId]{mt_rand(0, (strlen($characterListArray[$charListId]) - 1))};
         }
         shuffle($chars);
@@ -86,7 +98,8 @@ class TorneLIB_Crypto {
      *
      * @param $useKey
      */
-    public function setAesKey($useKey) {
+    public function setAesKey($useKey)
+    {
         $this->aesKey = md5($useKey);
     }
 
@@ -95,11 +108,21 @@ class TorneLIB_Crypto {
      *
      * @param $useIv
      */
-    public function setAesIv($useIv) {
+    public function setAesIv($useIv)
+    {
         $this->aesIv = md5($useIv);
     }
 
-    public function aesEncrypt($decryptedContent = "", $asBase64 = true) {
+    /**
+     * Encrypt content to RIJNDAEL/AES-encryption
+     *
+     * @param string $decryptedContent
+     * @param bool $asBase64
+     * @return string
+     * @throws TorneLIB_Exception
+     */
+    public function aesEncrypt($decryptedContent = "", $asBase64 = true)
+    {
         $useKey = $this->aesKey;
         $useIv = $this->aesIv;
         $contentData = $decryptedContent;
@@ -109,6 +132,7 @@ class TorneLIB_Crypto {
         if (is_string($decryptedContent)) {
             $contentData = utf8_encode($decryptedContent);
         }
+        // TODO: Need better support for PHP7
         $binEnc = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $useKey, $contentData, MCRYPT_MODE_CBC, $useIv);
         $baseEncoded = $this->base64url_encode($binEnc);
         if ($asBase64) {
@@ -117,37 +141,83 @@ class TorneLIB_Crypto {
             return $binEnc;
         }
     }
-    public function aesDecrypt($encryptedContent = "", $asBase64 = true) {
+
+    /**
+     * Decrypt content encoded with RIJNDAEL/AES-encryption
+     *
+     * @param string $encryptedContent
+     * @param bool $asBase64
+     * @return string
+     * @throws TorneLIB_Exception
+     */
+    public function aesDecrypt($encryptedContent = "", $asBase64 = true)
+    {
         $useKey = $this->aesKey;
         $useIv = $this->aesIv;
         if ($useKey == md5(md5("TorneLIB Default IV - Please Change this")) || $useIv == md5(md5("TorneLIB Default IV - Please Change this"))) {
             throw new TorneLIB_Exception("Current encryption key and iv is not allowed to use.", TORNELIB_EXCEPTIONS::TORNELIB_CRYPTO_KEY_EXCEPTION, __FUNCTION__);
         }
         $contentData = $encryptedContent;
-        if ($asBase64) { $contentData = $this->base64url_decode($encryptedContent); }
+        if ($asBase64) {
+            $contentData = $this->base64url_decode($encryptedContent);
+        }
         $decryptedOutput = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $useKey, $contentData, MCRYPT_MODE_CBC, $useIv));
         return $decryptedOutput;
     }
 
-    public function base64_gzencode($data = '', $compressionLevel = -1) {
+    /**
+     * Compress data with gzencode and encode to base64url
+     *
+     * @param string $data
+     * @param int $compressionLevel
+     * @return string
+     * @throws \Exception
+     */
+    public function base64_gzencode($data = '', $compressionLevel = -1)
+    {
         if (!function_exists('gzencode')) {
             throw new \Exception("Function gzencode is missing");
         }
         $gzEncoded = gzencode($data, $compressionLevel);
         return $this->base64url_encode($gzEncoded);
     }
-    public function base64_gzdecode($data = '') {
+
+    /**
+     * Decompress gzdata that has been encoded with base64url
+     * @param string $data
+     * @return string
+     */
+    public function base64_gzdecode($data = '')
+    {
         $gzDecoded = $this->base64url_decode($data);
         return $this->gzDecode($gzDecoded);
     }
-    public function base64_bzencode($data = '') {
+
+    /**
+     * Compress data with bzcompress and base64url-encode it
+     *
+     * @param string $data
+     * @return string
+     * @throws \Exception
+     */
+    public function base64_bzencode($data = '')
+    {
         if (!function_exists('bzcompress')) {
             throw new \Exception("bzcompress is missing");
         }
         $bzEncoded = bzcompress($data);
         return $this->base64url_encode($bzEncoded);
     }
-    public function base64_bzdecode($data) {
+
+    /**
+     * Decompress bzdata that has been encoded with base64url
+     *
+     * @param $data
+     * @return mixed
+     * @throws \Exception
+     */
+    public function base64_bzdecode($data)
+    {
         if (!function_exists('bzdecompress')) {
             throw new \Exception("bzdecompress is missing");
         }
@@ -156,12 +226,14 @@ class TorneLIB_Crypto {
     }
 
     /**
-     * Compress and encode with best encryption
+     * Compress and encode data with best encryption
      *
      * @param string $data
      * @return mixed
      */
-    public function base64_compress($data = '') {
+
+    public function base64_compress($data = '')
+    {
         $results = array();
         $bestCompression = null;
         $lengthArray = array();
@@ -182,6 +254,14 @@ class TorneLIB_Crypto {
         }
         return $results[$bestCompression];
     }
+
+    /**
+     * Decompress data that has been compressed with base64_compress
+     *
+     * @param string $data
+     * @param bool $getCompressionType
+     * @return string
+     */
     public function base64_decompress($data = '', $getCompressionType = false)
     {
         $results = array();
@@ -195,12 +275,16 @@ class TorneLIB_Crypto {
             if (isset($resultExploded[0]) && isset($resultExploded[1])) {
                 if ($resultExploded[0] == "gz0" || $resultExploded[0] == "gz9") {
                     $acceptedString = $resultExploded[1];
-                    if ($getCompressionType) {$acceptedString = $resultExploded[0];}
+                    if ($getCompressionType) {
+                        $acceptedString = $resultExploded[0];
+                    }
                     break;
                 }
                 if ($resultExploded[0] == "bz") {
                     $acceptedString = $resultExploded[1];
-                    if ($getCompressionType) {$acceptedString = $resultExploded[0];}
+                    if ($getCompressionType) {
+                        $acceptedString = $resultExploded[0];
+                    }
                     break;
                 }
             }
@@ -208,8 +292,15 @@ class TorneLIB_Crypto {
         return $acceptedString;
     }
 
-
-    private function gzDecode($data) {
+    /**
+     * Decode gzcompressed data. If gzdecode is actually missing (which has happened in early version of PHP), there will be a fallback to gzinflate instead
+     *
+     * @param $data
+     * @return string
+     * @throws \Exception
+     */
+    private function gzDecode($data)
+    {
         if (function_exists('gzdecode')) {
             return gzdecode($data);
         }
@@ -221,9 +312,8 @@ class TorneLIB_Crypto {
         $headerlen = 10;
         $extralen = 0;
         $filenamelen = 0;
-        if ($flags & 4)
-        {
-            $extralen = unpack('v' ,substr($data, 10, 2));
+        if ($flags & 4) {
+            $extralen = unpack('v', substr($data, 10, 2));
             $extralen = $extralen[1];
             $headerlen += 2 + $extralen;
         }
@@ -249,6 +339,7 @@ class TorneLIB_Crypto {
     {
         return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
     }
+
     /**
      * base64_decode
      *
