9f28bc3523ca8e5336c9bed08260a7d122f75034
Tomas Tornevall <tomas.tornevall@resurs.se>
2017-09-26 14:18:23 +0200

    EC-20170926, Test 0.4

--- tests/ecomphp-1.2.php
+++ tests/ecomphp-1.2.php
@@ -5,7 +5,7 @@
  *
  * @package EcomPHPTest
  * @author Resurs Bank Ecommrece <ecommerce.support@resurs.se>
- * @version 0.3
+ * @version 0.4
  * @link https://test.resurs.com/docs/x/KYM0 Get started - PHP Section
  * @license -
  *
