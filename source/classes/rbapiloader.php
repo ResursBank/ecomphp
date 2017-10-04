18d852171105a50cac5051286778d7a090c08f5c
Tomas Tornevall <tomas.tornevall@resurs.se>
2017-10-04 08:54:16 +0200

    ECOMPHP-136: getPaymentInvoices can't handle prepared objects

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -7094,9 +7094,15 @@ class ResursBank {
 	 * @since 1.1.11
 	 * @since 1.2.0
 	 */
-	public function getPaymentInvoices($paymentId = '') {
+	public function getPaymentInvoices($paymentIdOrPaymentObject = '') {
 		$invoices = array();
-		$paymentData = $this->getPayment($paymentId);
+		if (is_string($paymentIdOrPaymentObject)) {
+			$paymentData = $this->getPayment( $paymentIdOrPaymentObject );
+		} else if (is_object($paymentIdOrPaymentObject)) {
+			$paymentData = $paymentIdOrPaymentObject;
+		} else {
+			return array();
+		}
 		if (!empty($paymentData) && isset($paymentData->paymentDiffs)) {
 			foreach ($paymentData->paymentDiffs as $paymentRow) {
 				if (isset($paymentRow->type) && $paymentRow->type == "DEBIT" && isset($paymentRow->invoiceId)) {
