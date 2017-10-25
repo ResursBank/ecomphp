c992f402ce2378595afee7186ec85085dd93bbb3
Tomas Tornevall <tomas.tornevall@resurs.se>
2017-10-25 09:53:59 +0200

    ECOMPHP-151: classPath is obsolete in 1.2 and should not be there

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -10,7 +10,7 @@
  * @package RBEcomPHP
  * @author Resurs Bank Ecommerce <ecommerce.support@resurs.se>
  * @branch 1.1
- * @version 1.1.25
+ * @version 1.1.26
  * @link https://test.resurs.com/docs/x/KYM0 Get started - PHP Section
  * @link https://test.resurs.com/docs/x/TYNM EComPHP Usage
  * @license Apache License
@@ -223,9 +223,9 @@ class ResursBank {
 	////////// Private variables
 	///// Client Specific Settings
 	/** @var string The version of this gateway */
-	private $version = "1.1.25";
+	private $version = "1.1.26";
 	/** @var string Identify current version release (as long as we are located in v1.0.0beta this is necessary */
-	private $lastUpdate = "20171024";
+	private $lastUpdate = "20171025";
 	/** @var string This. */
 	private $clientName = "EComPHP";
 	/** @var string Replacing $clientName on usage of setClientNAme */
@@ -491,7 +491,11 @@ class ResursBank {
 	 * @deprecated Removed in 1.2
 	 */
 	private $classPath = "";
-	/** @var array Files to look for in class directories, to find RB */
+
+	/**
+	 * @var array Files to look for in class directories, to find RB
+	 * @deprecated Removed in 1.2
+	 */
 	private $classPathFiles = array(
 		'/simplifiedshopflowservice-client/Resurs_SimplifiedShopFlowService.php',
 		'/configurationservice-client/Resurs_ConfigurationService.php',
