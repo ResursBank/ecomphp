2e344ae8c7633f28c2987c9fe5b4972e162393df
Tomas Tornevall <tomas.tornevall@resurs.se>
2017-10-27 13:51:26 +0200

    ECOMPHP-162: getOrderStatusByPayment

--- source/classes/rbapiloader/ResursTypeClasses.php
+++ source/classes/rbapiloader/ResursTypeClasses.php
@@ -54,64 +54,18 @@ abstract class RESURS_CHECKOUT_CALL_TYPES {
  * @since 1.0.26
  * @since 1.1.26
  * @since 1.2.0
+ * @link https://test.resurs.com/docs/x/LAAF
  */
 abstract class RESURS_CALLBACK_TYPES {
-	/**
-	 * Callbacktype not defined
-	 */
-	const UNDEFINED = 0;
 
-	/**
-	 * Callback UNFREEZE
-	 *
-	 * Informs when an payment is unfrozen after manual fraud screening. This means that the payment may be debited (captured) and the goods can be delivered.
-	 * @link https://test.resurs.com/docs/display/ecom/UNFREEZE
-	 */
-	const UNFREEZE = 1;
-	/**
-	 * Callback ANNULMENT
-	 *
-	 * Will be sent once a payment is fully annulled at Resurs Bank, for example when manual fraud screening implies fraudulent usage. Annulling part of the payment will not trigger this event.
-	 * If the representative is not listening to this callback orders might be orphaned (i e without connected payment) and products bound to these orders never released.
-	 * @link https://test.resurs.com/docs/display/ecom/ANNULMENT
-	 */
-	const ANNULMENT = 2;
-	/**
-	 * Callback AUTOMATIC_FRAUD_CONTROL
-	 *
-	 * Will be sent once a payment is fully annulled at Resurs Bank, for example when manual fraud screening implies fraudulent usage. Annulling part of the payment will not trigger this event.
-	 * @link https://test.resurs.com/docs/display/ecom/AUTOMATIC_FRAUD_CONTROL
-	 */
-	const AUTOMATIC_FRAUD_CONTROL = 3;
-	/**
-	 * Callback FINALIZATION
-	 *
-	 * Once a payment is finalized automatically at Resurs Bank, for this will trigger this event, when the parameter finalizeIfBooked parmeter is set to true in paymentData. This callback will only be called if you are implementing the paymentData method with finilizedIfBooked parameter set to true, in the Simplified Shop Flow Service.
-	 * @link https://test.resurs.com/docs/display/ecom/FINALIZATION
-	 */
-	const FINALIZATION = 4;
-	/**
-	 * Callback TEST
-	 *
-	 * To test the callback mechanism. Can be used in integration testing to assure that communication works. A call is made to DeveloperService (triggerTestEvent) and Resurs Bank immediately does a callback. Note that TEST callback must be registered in the same way as all the other callbacks before it can be used.
-	 * @link https://test.resurs.com/docs/display/ecom/TEST
-	 */
-	const TEST = 5;
-	/**
-	 * Callback UPDATE
-	 *
-	 * Will be sent when a payment is updated. Resurs Bank will do a HTTP/POST call with parameter paymentId and the xml for paymentDiff to the registered URL.
-	 * @link https://test.resurs.com/docs/display/ecom/UPDATE
-	 */
-	const UPDATE = 6;
-
-	/**
-	 * Callback BOOKED
-	 *
-	 * Trigger: The order is in Resurs Bank system and ready for finalization
-	 * @link https://test.resurs.com/docs/display/ecom/BOOKED
-	 */
-	const BOOKED = 7;
+	const CALLBACK_TYPE_NOT_SET = 0;
+	const CALLBACK_TYPE_UNFREEZE = 1;
+	const CALLBACK_TYPE_ANNULMENT = 2;
+	const CALLBACK_TYPE_AUTOMATIC_FRAUD_CONTROL = 3;
+	const CALLBACK_TYPE_FINALIZATION = 4;
+	const CALLBACK_TYPE_TEST = 5;
+	const CALLBACK_TYPE_UPDATE = 6;
+	const CALLBACK_TYPE_BOOKED = 7;
 }
 
 /**
@@ -121,11 +75,11 @@ abstract class RESURS_CALLBACK_TYPES {
  * @since 1.2.0
  */
 abstract class RESURS_AFTERSHOP_RENDER_TYPES {
-	const NONE = 0;
-	const FINALIZE = 1;
-	const CREDIT = 2;
-	const ANNUL = 4;
-	const AUTHORIZE = 8;
+	const AFTERSHOP_NO_CHOICE = 0;
+	const AFTERSHOP_FINALIZE = 1;
+	const AFTERSHOP_CREDIT = 2;
+	const AFTERSHOP_ANNUL = 4;
+	const AFTERSHOP_AUTHORIZE = 8;
 }
 
 
@@ -158,18 +112,19 @@ abstract class RESURS_CALLBACK_REACHABILITY {
 }
 
 /**
- * Class RESURS_STATUS_RETURNCODES Order status return codes
+ * Class RESURS_PAYMENT_STATUS_RETURNCODES Order status return codes
  *
  * @since 1.0.26
  * @since 1.1.26
  * @since 1.2.0
  */
-abstract class RESURS_STATUS_RETURNCODES {
-	const PAYMENT_PENDING = 0;     // Waiting for callback or frozen
-	const PAYMENT_PROCESSING = 10;  // Booked, waiting for next action
-	const PAYMENT_COMPLETED = 20;   // Fully finalized (debited)
-	const PAYMENT_CANCELLED = 30;   // Fully annulled
-	const PAYMENT_REFUND = 40;      // Fully credited
+abstract class RESURS_PAYMENT_STATUS_RETURNCODES {
+	const PAYMENT_STATUS_COULD_NOT_BE_SET = 0;     // Waiting for callback or frozen
+	const PAYMENT_PENDING = 10;     // Waiting for callback or frozen
+	const PAYMENT_PROCESSING = 20;  // Booked, waiting for next action
+	const PAYMENT_COMPLETED = 30;   // Fully finalized (debited)
+	const PAYMENT_CANCELLED = 40;   // Fully annulled
+	const PAYMENT_REFUND = 50;      // Fully credited
 }
 
 ///
@@ -196,6 +151,17 @@ abstract class ResursCurlMethods extends RESURS_CURL_METHODS {}
 abstract class ResursAfterShopRenderTypes extends RESURS_AFTERSHOP_RENDER_TYPES
 {
 	/** @deprecated */
+	const NONE = 0;
+	/** @deprecated */
+	const FINALIZE = 1;
+	/** @deprecated */
+	const CREDIT = 2;
+	/** @deprecated */
+	const ANNUL = 4;
+	/** @deprecated */
+	const AUTHORIZE = 8;
+
+	/** @deprecated */
 	const UPDATE = 16;
 }
 
@@ -212,7 +178,75 @@ abstract class ResursCheckoutCallTypes extends RESURS_CHECKOUT_CALL_TYPES {}
  * @since 1.0.0
  * @deprecated RESURS_CALLBACK_TYPES
  */
-abstract class ResursCallbackTypes extends RESURS_CALLBACK_TYPES {}
+abstract class ResursCallbackTypes extends RESURS_CALLBACK_TYPES {
+	/**
+	 * Callbacktype not defined
+	 * @deprecated Use CALLBACK_TYPE_NOT_SET
+	 */
+	const UNDEFINED = 0;
+
+	/**
+	 * Callback UNFREEZE
+	 *
+	 * Informs when an payment is unfrozen after manual fraud screening. This means that the payment may be debited (captured) and the goods can be delivered.
+	 * @link https://test.resurs.com/docs/display/ecom/UNFREEZE
+	 * @deprecated Use CALLBACK_TYPE_UNFREEZE
+	 */
+	const UNFREEZE = 1;
+
+	/**
+	 * Callback ANNULMENT
+	 *
+	 * Will be sent once a payment is fully annulled at Resurs Bank, for example when manual fraud screening implies fraudulent usage. Annulling part of the payment will not trigger this event.
+	 * If the representative is not listening to this callback orders might be orphaned (i e without connected payment) and products bound to these orders never released.
+	 * @link https://test.resurs.com/docs/display/ecom/ANNULMENT
+	 * @deprecated Use CALLBACK_TYPE_ANNULMENT
+	 */
+	const ANNULMENT = 2;
+
+	/**
+	 * Callback AUTOMATIC_FRAUD_CONTROL
+	 *
+	 * Will be sent once a payment is fully annulled at Resurs Bank, for example when manual fraud screening implies fraudulent usage. Annulling part of the payment will not trigger this event.
+	 * @link https://test.resurs.com/docs/display/ecom/AUTOMATIC_FRAUD_CONTROL
+	 * @deprecated Use CALLBACK_TYPE_AUTOMATIC_FRAUD_CONTROL
+	 */
+	const AUTOMATIC_FRAUD_CONTROL = 3;
+
+	/**
+	 * Callback FINALIZATION
+	 *
+	 * Once a payment is finalized automatically at Resurs Bank, for this will trigger this event, when the parameter finalizeIfBooked parmeter is set to true in paymentData. This callback will only be called if you are implementing the paymentData method with finilizedIfBooked parameter set to true, in the Simplified Shop Flow Service.
+	 * @link https://test.resurs.com/docs/display/ecom/FINALIZATION
+	 * @deprecated Use CALLBACK_TYPE_FINALIZATION
+	 */
+	const FINALIZATION = 4;
+
+	/**
+	 * Callback TEST
+	 *
+	 * To test the callback mechanism. Can be used in integration testing to assure that communication works. A call is made to DeveloperService (triggerTestEvent) and Resurs Bank immediately does a callback. Note that TEST callback must be registered in the same way as all the other callbacks before it can be used.
+	 * @link https://test.resurs.com/docs/display/ecom/TEST
+	 * @deprecated CALLBACK_TYPE_TEST
+	 */
+	const TEST = 5;
+	/**
+	 * Callback UPDATE
+	 *
+	 * Will be sent when a payment is updated. Resurs Bank will do a HTTP/POST call with parameter paymentId and the xml for paymentDiff to the registered URL.
+	 * @link https://test.resurs.com/docs/display/ecom/UPDATE
+	 * @deprecated Use CALLBACK_TYPE_UPDATE
+	 */
+	const UPDATE = 6;
+
+	/**
+	 * Callback BOOKED
+	 *
+	 * Trigger: The order is in Resurs Bank system and ready for finalization
+	 * @link https://test.resurs.com/docs/display/ecom/BOOKED
+	 * @deprecated Use CALLBACK_TYPE_BOOKED
+	 */
+	const BOOKED = 7;}
 
 /**
  * Class ResursMethodTypes Preferred payment method types if called.
