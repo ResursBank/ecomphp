3c53b6a92bcec3072c43f2ae7ba064b21baa897d
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-01-23 11:24:54 +0100

    exception doc

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -3346,7 +3346,7 @@ class ResursBank {
 		$error  = array();
 		$myFlow = $this->getPreferredPaymentFlowService();
 
-		$this->addMetaDataHash($payment_id_or_method);
+		//$this->addMetaDataHash($payment_id_or_method);
 
 		// Using this function to validate that card data info is properly set up during the deprecation state in >= 1.0.2/1.1.1
 		if ( $myFlow == RESURS_FLOW_TYPES::FLOW_SIMPLIFIED_FLOW ) {
@@ -3421,6 +3421,8 @@ class ResursBank {
 	/**
 	 * @param $paymentId
 	 * @param int $hashLevel
+	 *
+	 * @throws \Exception
 	 */
 	public function addMetaDataHash($paymentId, $hashLevel = RESURS_METAHASH_TYPES::HASH_ORDERLINES) {
 		if (!$this->metaDataHashEnabled) {return;}
