207b885d8115c7c33a1872f6e17f49b09c4309dc
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-04-03 15:15:33 +0200

    ECOMPHP-221: Modularize forms and regex handlers for 1.2/1.3

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -7,7 +7,7 @@
  * @package RBEcomPHP
  * @author Resurs Bank Ecommerce <ecommerce.support@resurs.se>
  * @branch 1.3
- * @version 1.3.8
+ * @version 1.3.9
  * @link https://test.resurs.com/docs/x/KYM0 Get started - PHP Section
  * @link https://test.resurs.com/docs/x/TYNM EComPHP Usage
  * @license Apache License
@@ -15,16 +15,15 @@
 
 namespace Resursbank\RBEcomPHP;
 
-/**
- * Location of RBEcomPHP class files.
- */
-if ( ! defined( 'RB_API_PATH' ) ) {
-	define( 'RB_API_PATH', __DIR__ );
-}
-require_once( RB_API_PATH . '/rbapiloader/ResursTypeClasses.php' );
-require_once( RB_API_PATH . '/rbapiloader/ResursException.php' );
-if ( file_exists( RB_API_PATH . "/../../vendor/autoload.php" ) ) {
-	require_once( RB_API_PATH . '/../../vendor/autoload.php' );
+// Prevent duplicate loading
+if ( class_exists( 'ResursBank' ) && class_exists( 'Resursbank\RBEcomPHP\ResursBank' ) ) {return;}
+
+require_once( __DIR__ . '/rbapiloader/ResursForms.php' );
+require_once( __DIR__ . '/rbapiloader/ResursTypeClasses.php' );
+require_once( __DIR__ . '/rbapiloader/ResursException.php' );
+
+if ( file_exists( __DIR__ . "/../../vendor/autoload.php" ) ) {
+	require_once( __DIR__ . '/../../vendor/autoload.php' );
 }
 
 use \TorneLIB\TorneLIB_Crypto;
@@ -36,8 +35,12 @@ use \TorneLIB\CURL_POST_AS;
 /*
  *  Global
  */
-define('ECOMPHP_VERSION', '1.3.8');
-define('ECOMPHP_MODIFY_DATE', '20180320');
+if ( ! defined( 'ECOMPHP_VERSION' ) ) {
+	define( 'ECOMPHP_VERSION', '1.3.9' );
+}
+if ( ! defined( 'ECOMPHP_MODIFY_DATE' ) ) {
+	define( 'ECOMPHP_MODIFY_DATE', '20180403' );
+}
 
 /**
  * Class ResursBank Primary class for EComPHP
@@ -174,6 +177,10 @@ class ResursBank {
 	 * @since 1.2.0
 	 */
 	private $T_CRYPTO;
+
+	/** @var RESURS_DEPRECATED_FLOW $E_DEPRECATED */
+	private $E_DEPRECATED;
+
 	/**
 	 * The payload rendered out from CreatePayment()
 	 * @var array
@@ -404,6 +411,7 @@ class ResursBank {
 			$this->setEnvironment( $targetEnvironment );
 		}
 		$this->setUserAgent();
+		$this->E_DEPRECATED = new RESURS_DEPRECATED_FLOW();
 
 	}
 
@@ -2867,145 +2875,22 @@ class ResursBank {
 	 * @param $fieldArray
 	 *
 	 * @return array
-	 * @deprecated 1.0.8 It is strongly recommended that you are generating all this by yourself in an integratio
-	 * @deprecated 1.1.8 It is strongly recommended that you are generating all this by yourself in an integration
+	 * @deprecated 1.0.8 Build your own integration please
+	 * @deprecated 1.1.8 Build your own integration please
 	 */
 	public function setFormTemplateRules( $customerType, $methodType, $fieldArray ) {
-		$this->formTemplateRuleArray = array(
-			$customerType => array(
-				'fields' => array(
-					$methodType => $fieldArray
-				)
-			)
-		);
-
-		return $this->formTemplateRuleArray;
+		return $this->E_DEPRECATED->setFormTemplateRules($customerType, $methodType, $fieldArray);
 	}
 
 	/**
 	 * Retrieve html-form rules for each payment method type, including regular expressions for the form fields, to validate against.
 	 *
 	 * @return array
-	 * @deprecated 1.0.8 It is strongly recommended that you are generating all this by yourself in an integration.
-	 * @deprecated 1.1.8 It is strongly recommended that you are generating all this by yourself in an integration.
+	 * @deprecated 1.0.8 Build your own integration please
+	 * @deprecated 1.1.8 Build your own integration please
 	 */
 	private function getFormTemplateRules() {
-		$formTemplateRules = array(
-			'NATURAL' => array(
-				'fields' => array(
-					'INVOICE'          => array(
-						'applicant-government-id',
-						'applicant-telephone-number',
-						'applicant-mobile-number',
-						'applicant-email-address'
-					),
-					'CARD'             => array( 'applicant-government-id', 'card-number' ),
-					'REVOLVING_CREDIT' => array(
-						'applicant-government-id',
-						'applicant-telephone-number',
-						'applicant-mobile-number',
-						'applicant-email-address'
-					),
-					'PART_PAYMENT'     => array(
-						'applicant-government-id',
-						'applicant-telephone-number',
-						'applicant-mobile-number',
-						'applicant-email-address'
-					)
-				)
-			),
-			'LEGAL'   => array(
-				'fields' => array(
-					'INVOICE' => array(
-						'applicant-government-id',
-						'applicant-telephone-number',
-						'applicant-mobile-number',
-						'applicant-email-address',
-						'applicant-full-name',
-						'contact-government-id'
-					),
-				)
-			),
-			'display' => array(
-				'applicant-government-id',
-				'card-number',
-				'applicant-full-name',
-				'contact-government-id'
-			),
-			'regexp'  => array(
-				'SE' => array(
-					'NATURAL' => array(
-						'applicant-government-id'    => '^(18\d{2}|19\d{2}|20\d{2}|\d{2})(0[1-9]|1[0-2])([0][1-9]|[1-2][0-9]|3[0-1])(\-|\+)?([\d]{4})$',
-						'applicant-telephone-number' => '^(0|\+46|0046)[ |-]?(200|20|70|73|76|74|[1-9][0-9]{0,2})([ |-]?[0-9]){5,8}$',
-						'applicant-mobile-number'    => '^(0|\+46|0046)[ |-]?(200|20|70|73|76|74|[1-9][0-9]{0,2})([ |-]?[0-9]){5,8}$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					),
-					'LEGAL'   => array(
-						'applicant-government-id'    => '^(16\d{2}|18\d{2}|19\d{2}|20\d{2}|\d{2})(\d{2})(\d{2})(\-|\+)?([\d]{4})$',
-						'applicant-telephone-number' => '^(0|\+46|0046)[ |-]?(200|20|70|73|76|74|[1-9][0-9]{0,2})([ |-]?[0-9]){5,8}$',
-						'applicant-mobile-number'    => '^(0|\+46|0046)[ |-]?(200|20|70|73|76|74|[1-9][0-9]{0,2})([ |-]?[0-9]){5,8}$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					)
-				),
-				'DK' => array(
-					'NATURAL' => array(
-						'applicant-government-id'    => '^((3[0-1])|([1-2][0-9])|(0[1-9]))((1[0-2])|(0[1-9]))(\d{2})(\-)?([\d]{4})$',
-						'applicant-telephone-number' => '^(\+45|0045|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-mobile-number'    => '^(\+45|0045|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					),
-					'LEGAL'   => array(
-						'applicant-government-id'    => null,
-						'applicant-telephone-number' => '^(\+45|0045|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-mobile-number'    => '^(\+45|0045|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					)
-				),
-				'NO' => array(
-					'NATURAL' => array(
-						'applicant-government-id'    => '^([0][1-9]|[1-2][0-9]|3[0-1])(0[1-9]|1[0-2])(\d{2})(\-)?([\d]{5})$',
-						'applicant-telephone-number' => '^(\+47|0047|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-mobile-number'    => '^(\+47|0047|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					),
-					'LEGAL'   => array(
-						'applicant-government-id'    => '^([89]([ |-]?[0-9]){8})$',
-						'applicant-telephone-number' => '^(\+47|0047|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-mobile-number'    => '^(\+47|0047|)?[ |-]?[2-9]([ |-]?[0-9]){7,7}$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					)
-				),
-				'FI' => array(
-					'NATURAL' => array(
-						'applicant-government-id'    => '^([\d]{6})[\+\-A]([\d]{3})([0123456789ABCDEFHJKLMNPRSTUVWXY])$',
-						'applicant-telephone-number' => '^((\+358|00358|0)[-| ]?(1[1-9]|[2-9]|[1][0][1-9]|201|2021|[2][0][2][4-9]|[2][0][3-8]|29|[3][0][1-9]|71|73|[7][5][0][0][3-9]|[7][5][3][0][3-9]|[7][5][3][2][3-9]|[7][5][7][5][3-9]|[7][5][9][8][3-9]|[5][0][0-9]{0,2}|[4][0-9]{1,3})([-| ]?[0-9]){3,10})?$',
-						'applicant-mobile-number'    => '^((\+358|00358|0)[-| ]?(1[1-9]|[2-9]|[1][0][1-9]|201|2021|[2][0][2][4-9]|[2][0][3-8]|29|[3][0][1-9]|71|73|[7][5][0][0][3-9]|[7][5][3][0][3-9]|[7][5][3][2][3-9]|[7][5][7][5][3-9]|[7][5][9][8][3-9]|[5][0][0-9]{0,2}|[4][0-9]{1,3})([-| ]?[0-9]){3,10})?$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					),
-					'LEGAL'   => array(
-						'applicant-government-id'    => '^((\d{7})(\-)?\d)$',
-						'applicant-telephone-number' => '^((\+358|00358|0)[-| ]?(1[1-9]|[2-9]|[1][0][1-9]|201|2021|[2][0][2][4-9]|[2][0][3-8]|29|[3][0][1-9]|71|73|[7][5][0][0][3-9]|[7][5][3][0][3-9]|[7][5][3][2][3-9]|[7][5][7][5][3-9]|[7][5][9][8][3-9]|[5][0][0-9]{0,2}|[4][0-9]{1,3})([-| ]?[0-9]){3,10})?$',
-						'applicant-mobile-number'    => '^((\+358|00358|0)[-| ]?(1[1-9]|[2-9]|[1][0][1-9]|201|2021|[2][0][2][4-9]|[2][0][3-8]|29|[3][0][1-9]|71|73|[7][5][0][0][3-9]|[7][5][3][0][3-9]|[7][5][3][2][3-9]|[7][5][7][5][3-9]|[7][5][9][8][3-9]|[5][0][0-9]{0,2}|[4][0-9]{1,3})([-| ]?[0-9]){3,10})?$',
-						'applicant-email-address'    => '^[A-Za-z0-9!#%&\'*+/=?^_`~-]+(\.[A-Za-z0-9!#%&\'*+/=?^_`~-]+)*@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$',
-						'card-number'                => '^([1-9][0-9]{3}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4}[ ]{0,1}[0-9]{4})$'
-					)
-				),
-			)
-		);
-		if ( isset( $this->formTemplateRuleArray ) && is_array( $this->formTemplateRuleArray ) && count( $this->formTemplateRuleArray ) ) {
-			foreach ( $this->formTemplateRuleArray as $cType => $cArray ) {
-				$formTemplateRules[ $cType ] = $cArray;
-			}
-		}
-
-		return $formTemplateRules;
+		return $this->E_DEPRECATED->getFormTemplateRules();
 	}
 
 	/**
@@ -3020,46 +2905,11 @@ class ResursBank {
 	 *
 	 * @return array
 	 * @throws \Exception
-	 * @deprecated 1.0.8 It is strongly recommended that you are generating all this by yourself in an integration
-	 * @deprecated 1.1.8 It is strongly recommended that you are generating all this by yourself in an integration
+	 * @deprecated 1.0.8 Build your own integration please
+	 * @deprecated 1.1.8 Build your own integration please
 	 */
 	public function getRegEx( $formFieldName = '', $countryCode, $customerType ) {
-		$returnRegEx = array();
-
-		$templateRule = $this->getFormTemplateRules();
-		$returnRegEx  = $templateRule['regexp'];
-
-		if ( empty( $countryCode ) ) {
-			throw new \Exception( __FUNCTION__ . ": Country code is missing in getRegEx-request for form fields", \RESURS_EXCEPTIONS::REGEX_COUNTRYCODE_MISSING );
-		}
-		if ( empty( $customerType ) ) {
-			throw new \Exception( __FUNCTION__ . ": Customer type is missing in getRegEx-request for form fields", \RESURS_EXCEPTIONS::REGEX_CUSTOMERTYPE_MISSING );
-		}
-
-		if ( ! empty( $countryCode ) && isset( $returnRegEx[ strtoupper( $countryCode ) ] ) ) {
-			$returnRegEx = $returnRegEx[ strtoupper( $countryCode ) ];
-			if ( ! empty( $customerType ) ) {
-				if ( ! is_array( $customerType ) ) {
-					if ( isset( $returnRegEx[ strtoupper( $customerType ) ] ) ) {
-						$returnRegEx = $returnRegEx[ strtoupper( $customerType ) ];
-						if ( isset( $returnRegEx[ strtolower( $formFieldName ) ] ) ) {
-							$returnRegEx = $returnRegEx[ strtolower( $formFieldName ) ];
-						}
-					}
-				} else {
-					foreach ( $customerType as $cType ) {
-						if ( isset( $returnRegEx[ strtoupper( $cType ) ] ) ) {
-							$returnRegEx = $returnRegEx[ strtoupper( $cType ) ];
-							if ( isset( $returnRegEx[ strtolower( $formFieldName ) ] ) ) {
-								$returnRegEx = $returnRegEx[ strtolower( $formFieldName ) ];
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return $returnRegEx;
+		return $this->E_DEPRECATED->getRegEx($formFieldName, $countryCode, $customerType);
 	}
 
 	/**
@@ -3073,29 +2923,11 @@ class ResursBank {
 	 *
 	 * @return bool Returns false if you should NOT hide the field
 	 * @throws \Exception
-	 * @deprecated 1.0.8 It is strongly recommended that you are generating all this by yourself in an integration
-	 * @deprecated 1.1.8 It is strongly recommended that you are generating all this by yourself in an integration
+	 * @deprecated 1.0.8 Build your own integration please
+	 * @deprecated 1.1.8 Build your own integration please
 	 */
 	public function canHideFormField( $formField = "", $canThrow = false ) {
-		$canHideSet = false;
-
-		if ( is_array( $this->templateFieldsByMethodResponse ) && count( $this->templateFieldsByMethodResponse ) && isset( $this->templateFieldsByMethodResponse['fields'] ) && isset( $this->templateFieldsByMethodResponse['display'] ) ) {
-			$currentDisplay = $this->templateFieldsByMethodResponse['display'];
-			if ( in_array( $formField, $currentDisplay ) ) {
-				$canHideSet = false;
-			} else {
-				$canHideSet = true;
-			}
-		} else {
-			/* Make sure that we don't hide things that does not exists in our configuration */
-			$canHideSet = false;
-		}
-
-		if ( $canThrow && ! $canHideSet ) {
-			throw new \Exception( __FUNCTION__ . ": templateFieldsByMethodResponse is empty. You have to run getTemplateFieldsByMethodType first", \RESURS_EXCEPTIONS::FORMFIELD_CANHIDE_EXCEPTION );
-		}
-
-		return $canHideSet;
+		return $this->E_DEPRECATED->canHideFormField($formField, $canThrow);
 	}
 
 	/**
@@ -3116,45 +2948,11 @@ class ResursBank {
 	 * @param string $specificType
 	 *
 	 * @return array
-	 * @deprecated 1.0.8 It is strongly recommended that you are generating all this by yourself in an integration
-	 * @deprecated 1.1.8 It is strongly recommended that you are generating all this by yourself in an integration
+	 * @deprecated 1.0.8 Build your own integration please
+	 * @deprecated 1.1.8 Build your own integration please
 	 */
 	public function getTemplateFieldsByMethodType( $paymentMethodName = "", $customerType = "", $specificType = "" ) {
-		$templateRules     = $this->getFormTemplateRules();
-		$returnedRules     = array();
-		$returnedRuleArray = array();
-		/* If the client is requesting a getPaymentMethod-object we'll try to handle that information instead (but not if it is empty) */
-		if ( is_object( $paymentMethodName ) || is_array( $paymentMethodName ) ) {
-			if ( is_object( $paymentMethodName ) ) {
-				// Prevent arrays to go through here and crash something
-				if ( ! is_array( $customerType ) ) {
-					if ( isset( $templateRules[ strtoupper( $customerType ) ] ) && isset( $templateRules[ strtoupper( $customerType ) ]['fields'][ strtoupper( $paymentMethodName->specificType ) ] ) ) {
-						$returnedRuleArray = $templateRules[ strtoupper( $customerType ) ]['fields'][ strtoupper( $paymentMethodName->specificType ) ];
-					}
-				}
-			} else if ( is_array( $paymentMethodName ) ) {
-				/*
-				 * This should probably not happen and the developers should probably also stick to objects as above.
-				 */
-				if ( is_array( $paymentMethodName ) && count( $paymentMethodName ) ) {
-					if ( isset( $templateRules[ strtoupper( $customerType ) ] ) && isset( $templateRules[ strtoupper( $customerType ) ]['fields'][ strtoupper( $paymentMethodName['specificType'] ) ] ) ) {
-						$returnedRuleArray = $templateRules[ strtoupper( $customerType ) ]['fields'][ strtoupper( $paymentMethodName['specificType'] ) ];
-					}
-				}
-			}
-		} else {
-			if ( isset( $templateRules[ strtoupper( $customerType ) ] ) && isset( $templateRules[ strtoupper( $customerType ) ]['fields'][ strtoupper( $paymentMethodName ) ] ) ) {
-				$returnedRuleArray = $templateRules[ strtoupper( $customerType ) ]['fields'][ strtoupper( $specificType ) ];
-			}
-		}
-		$returnedRules                        = array(
-			'fields'  => $returnedRuleArray,
-			'display' => $templateRules['display'],
-			'regexp'  => $templateRules['regexp']
-		);
-		$this->templateFieldsByMethodResponse = $returnedRules;
-
-		return $returnedRules;
+		return $this->E_DEPRECATED->getTemplateFieldsByMethodType($paymentMethodName, $customerType, $specificType);
 	}
 
 	/**
@@ -3164,11 +2962,11 @@ class ResursBank {
 	 *
 	 * @return array
 	 * @throws \Exception
-	 * @deprecated 1.0.8 It is strongly recommended that you are generating all this by yourself in an integration
-	 * @deprecated 1.1.8 It is strongly recommended that you are generating all this by yourself in an integration
+	 * @deprecated 1.0.8 Build your own integration please
+	 * @deprecated 1.1.8 Build your own integration please
 	 */
 	public function getTemplateFieldsByMethod( $paymentMethodName = "" ) {
-		return $this->getTemplateFieldsByMethodType( $this->getPaymentMethodSpecific( $paymentMethodName ) );
+		return $this->E_DEPRECATED->getTemplateFieldsByMethodType( $this->getPaymentMethodSpecific( $paymentMethodName ) );
 	}
 
 	/**
@@ -3178,11 +2976,11 @@ class ResursBank {
 	 *
 	 * @return array
 	 * @throws \Exception
-	 * @deprecated 1.0.8 It is strongly recommended that you are generating all this by yourself in an integration
-	 * @deprecated 1.1.8 It is strongly recommended that you are generating all this by yourself in an integration
+	 * @deprecated 1.0.8 Build your own integration please
+	 * @deprecated 1.1.8 Build your own integration please
 	 */
 	public function getFormFieldsByMethod( $paymentMethodName = "" ) {
-		return $this->getTemplateFieldsByMethod( $paymentMethodName );
+		return $this->E_DEPRECATED->getTemplateFieldsByMethod( $paymentMethodName );
 	}
 
 
