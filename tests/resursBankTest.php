cb3b0606d74ef0cfe4f750bc1aa0d18d7b503289
Tomas Tornevall <thorne@tornevall.net>
2018-04-25 07:56:17 +0200

    Parsing SOAP body from 6.0.19 and lower requires other ways

--- tests/resursBankTest.php
+++ tests/resursBankTest.php
@@ -40,6 +40,7 @@ use TorneLIB\NETCURL_PARSER;
 use TorneLIB\TorneLIB_IO;
 use \TorneLIB\Tornevall_cURL;
 use \TorneLIB\TorneLIB_Network;
+use TorneLIB\Tornevall_SimpleSoap;
 
 // Global test configuration section starts here
 require_once( __DIR__ . "/classes/ResursBankTestClass.php" );
@@ -81,8 +82,8 @@ class resursBankTest extends TestCase {
 	private $signUrl = "https://test.resurs.com/signdummy/index.php?isSigningUrl=1";
 
 	function setUp() {
-		$this->API  = new ResursBank();
-		$this->API->setDebug(true);
+		$this->API = new ResursBank();
+		$this->API->setDebug( true );
 		$this->TEST = new RESURS_TEST_BRIDGE();
 	}
 
@@ -187,25 +188,41 @@ class resursBankTest extends TestCase {
 
 		return $happyCustomer;
 	}
+
 	/**
 	 * @test
 	 * @testdox getCurlHandle (using getAddress)
 	 */
 	function getAddressCurlHandle() {
-		if (!class_exists('\SimpleXMLElement')) {
-			static::markTestSkipped("SimpleXMLElement missing");
+		if ( ! class_exists( '\SimpleXMLElement' ) ) {
+			static::markTestSkipped( "SimpleXMLElement missing" );
 		}
 
 		$this->TEST->ECOM->getAddress( $this->flowHappyCustomer );
 		/** @var Tornevall_cURL $lastCurlHandle */
-		$lastCurlHandle = $this->TEST->ECOM->getCurlHandle();
+
+		if ( defined( 'TORNELIB_NETCURL_RELEASE' ) && version_compare( TORNELIB_NETCURL_RELEASE, '6.0.20', '<' ) ) {
+			// In versions prior to 6.0.20, you need to first extract the SOAP body from simpleSoap itself (via getLibResponse).
+			$lastCurlHandle = $this->TEST->ECOM->getCurlHandle( true );
+			/** @var Tornevall_SimpleSoap $lastCurlHandle */
+			$soapLibResponse = $lastCurlHandle->getLibResponse();
+			$selfParser      = new TorneLIB_IO();
+			$byIo            = $selfParser->getFromXml( $soapLibResponse['body'], true );
+			static::assertTrue( ( $byIo->fullName == $this->flowHappyCustomerName ? true : false ) && ( $soapLibResponse['parsed']->fullName == $this->flowHappyCustomerName ? true : false ) );
+
+			return;
+		}
 
 		// The XML parser in the IO MODULE should give the same response as the direct curl handle
+		// From NetCURL 6.0.20 and the IO library, this could be extracted directly from the curl handle
 		$selfParser = new TorneLIB_IO();
-		$byIo = $selfParser->getFromXml($lastCurlHandle->getResponseBody(), true);
+		// Get the curl handle without bulk request
+		$lastCurlHandle = $this->TEST->ECOM->getCurlHandle();
+
+		$byIo     = $selfParser->getFromXml( $lastCurlHandle->getResponseBody(), true );
 		$byHandle = $lastCurlHandle->getParsedResponse();
 
-		static::assertTrue($byIo->fullName == $this->flowHappyCustomerName && $byHandle->fullName == $this->flowHappyCustomerName);
+		static::assertTrue( $byIo->fullName == $this->flowHappyCustomerName && $byHandle->fullName == $this->flowHappyCustomerName );
 	}
 
 	/**
