e4b0fd53f687b02e3a745aedb9d1396318852d7b
Tomas Tornevall <thorne@tornevall.net>
2018-08-14 22:17:57 +0200

    Rearranged code

--- tests/curlTest.php
+++ tests/curlTest.php
@@ -2,20 +2,21 @@
 
 namespace TorneLIB;
 
-if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
-	require_once( __DIR__ . '/../vendor/autoload.php' );
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../vendor/autoload.php');
 }
-if ( file_exists( __DIR__ . "/../tornelib.php" ) ) {
-	// Work with TorneLIBv5
+if (file_exists(__DIR__ . "/../tornelib.php")) {
+    // Work with TorneLIBv5
     /** @noinspection PhpIncludeInspection */
-    require_once(__DIR__ . '/../tornelib.php' );
+    require_once(__DIR__ . '/../tornelib.php');
 }
 
 use PHPUnit\Framework\TestCase;
 
-ini_set( 'memory_limit', - 1 );    // Free memory limit, some tests requires more memory (like ip-range handling)
+ini_set('memory_limit', -1);    // Free memory limit, some tests requires more memory (like ip-range handling)
 
 /** @noinspection PhpUndefinedClassInspection */
+
 class curlTest extends TestCase
 {
     /**
@@ -27,12 +28,42 @@ class curlTest extends TestCase
     private $wsdl = "https://test.resurs.com/ecommerce-test/ws/V4/SimplifiedShopFlowService?wsdl";
 
     /**
+     * @test
+     * @testdox Testing an error type that comes from this specific service - testing if we can catch previous error
+     *          instead of the current
      * @throws \Exception
      */
-    function setUp()
+    public function soapFaultstring()
     {
-        error_reporting(E_ALL);
-        $this->CURL = new MODULE_CURL();
+        $this->disableSslVerifyByPhpVersions(true);
+        try {
+            $wsdl = $this->CURL->doGet($this->wsdl);
+            /** @noinspection PhpUndefinedMethodInspection */
+            $wsdl->getPaymentMethods();
+        } catch (\Exception $e) {
+            $previousException = $e->getPrevious();
+
+            if ($e->getCode() < 3) {
+                static::markTestSkipped(
+                    'Getting exception codes below 3 here, might indicate that your cacerts is not installed properly or the connection to the server is not responding'
+                );
+
+                return;
+            } elseif ($e->getCode() >= 500) {
+                static::markTestSkipped(
+                    "Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage()
+                );
+            }
+
+            static::assertTrue(
+                isset($previousException->faultstring) &&
+                !empty($previousException->faultstring) &&
+                preg_match(
+                    "/unauthorized/i",
+                    $e->getMessage()
+                )
+            );
+        }
     }
 
     /**
@@ -40,7 +71,6 @@ class curlTest extends TestCase
      * For the tests, where the importance of result is not focused on SSL, we could disable the verification
      * checks if we want to do so. In Bitbucket Pipelines docker environments errors has been discovered on
      * some PHP releases, which we'd like to primary disable.
-     *
      * @param bool $always
      */
     private function disableSslVerifyByPhpVersions($always = false)
@@ -54,39 +84,10 @@ class curlTest extends TestCase
 
     /**
      * @test
-     * @testdox Testing an error type that comes from this specific service - testing if we can catch previous error
-     *          instead of the current
-     * @throws \Exception
-     */
-    function soapFaultstring()
-    {
-        $this->disableSslVerifyByPhpVersions(true);
-        try {
-            $wsdl = $this->CURL->doGet($this->wsdl);
-            /** @noinspection PhpUndefinedMethodInspection */
-            $wsdl->getPaymentMethods();
-        } catch (\Exception $e) {
-            $previousException = $e->getPrevious();
-
-            if ($e->getCode() < 3) {
-                static::markTestSkipped('Getting exception codes below 3 here, might indicate that your cacerts is not installed properly or the connection to the server is not responding');
-
-                return;
-            } elseif ($e->getCode() >= 500) {
-                static::markTestSkipped("Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage());
-            }
-
-            static::assertTrue(isset($previousException->faultstring) && ! empty($previousException->faultstring) && preg_match("/unauthorized/i",
-                    $e->getMessage()));
-        }
-    }
-
-    /**
-     * @test
      * @testdox Testing unauthorized request by regular request (should give the same repsonse as soapFaultString)
      * @throws \Exception
      */
-    function soapUnauthorizedSoapUnauthorized()
+    public function soapUnauthorizedSoapUnauthorized()
     {
         try {
             $this->disableSslVerifyByPhpVersions();
@@ -95,17 +96,21 @@ class curlTest extends TestCase
             $wsdl->getPaymentMethods();
         } catch (\Exception $e) {
             $exMessage = $e->getMessage();
-            $exCode    = $e->getCode();
+            $exCode = $e->getCode();
 
             $isUnCode = $exCode == 401 ? true : false;
             $isUnText = preg_match("/unauthorized/i", $exMessage) ? true : false;
 
             if ((intval($exCode) <= 3 && intval($exCode) > 0) || preg_match("/14090086/", $exMessage)) {
-                static::markTestSkipped('Getting exception codes below 3 here, might indicate that your cacerts is not installed properly or the connection to the server is not responding');
+                static::markTestSkipped(
+                    'Getting exception codes below 3 here, might indicate that your cacerts is not installed properly or the connection to the server is not responding'
+                );
 
                 return;
             } elseif ($exCode >= 500) {
-                static::markTestSkipped("Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage());
+                static::markTestSkipped(
+                    "Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage()
+                );
             }
 
             static::assertTrue(($isUnCode || $isUnText) ? true : false);
@@ -117,7 +122,7 @@ class curlTest extends TestCase
      * @testdox Test soapfaults when authentication are set up (as this generates other errors than without auth set)
      * @throws \Exception
      */
-    function soapAuthErrorInitialSoapFaultsWsdl()
+    public function soapAuthErrorInitialSoapFaultsWsdl()
     {
         if (version_compare(PHP_VERSION, '5.4.0', '<')) {
             $this->CURL->setChain(false);
@@ -132,7 +137,7 @@ class curlTest extends TestCase
             $wsdl->getPaymentMethods();
         } catch (\Exception $e) {
             $errorMessage = $e->getMessage();
-            $errorCode    = $e->getCode();
+            $errorCode = $e->getCode();
 
             if (preg_match('/this when not in object context/i', $errorMessage)) {
                 static::markTestSkipped('This test might not support chaining: ' . $errorMessage);
@@ -145,11 +150,15 @@ class curlTest extends TestCase
                 $assertThis = true;
             }
             if (intval($errorCode) == 2) {
-                static::markTestSkipped("Possible SSL3_GET_SERVER_CERTIFICATE - If you run this test, make sure the certificate verification works");
+                static::markTestSkipped(
+                    "Possible SSL3_GET_SERVER_CERTIFICATE - If you run this test, make sure the certificate verification works"
+                );
 
                 return;
             } elseif ($errorCode >= 500) {
-                static::markTestSkipped("Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage());
+                static::markTestSkipped(
+                    "Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage()
+                );
             }
             static::assertTrue($assertThis, $errorMessage . " (" . $errorCode . ")");
         }
@@ -160,19 +169,21 @@ class curlTest extends TestCase
      * @testdox Post as SOAP, without the wsdl prefix
      * @throws \Exception
      */
-    function soapAuthErrorInitialSoapFaultsNoWsdl()
+    public function soapAuthErrorInitialSoapFaultsNoWsdl()
     {
         $this->disableSslVerifyByPhpVersions();
         $this->CURL->setSoapTryOnce(false);
         $this->CURL->setAuthentication("fail", "fail");
         try {
-            $wsdl = $this->CURL->doGet('https://test.resurs.com/ecommerce-test/ws/V4/SimplifiedShopFlowService',
-                NETCURL_POST_DATATYPES::DATATYPE_SOAP);
+            $wsdl = $this->CURL->doGet(
+                'https://test.resurs.com/ecommerce-test/ws/V4/SimplifiedShopFlowService',
+                NETCURL_POST_DATATYPES::DATATYPE_SOAP
+            );
             /** @noinspection PhpUndefinedMethodInspection */
             $wsdl->getPaymentMethods();
         } catch (\Exception $e) {
             $errorMessage = $e->getMessage();
-            $errorCode    = $e->getCode();
+            $errorCode = $e->getCode();
 
             if (preg_match('/this when not in object context/i', $errorMessage)) {
                 static::markTestSkipped('This test might not support chaining: ' . $errorMessage);
@@ -185,11 +196,15 @@ class curlTest extends TestCase
                 $assertThis = true;
             }
             if (intval($errorCode) == 2) {
-                static::markTestSkipped("Possible SSL3_GET_SERVER_CERTIFICATE - If you run this test, make sure the certificate verification works");
+                static::markTestSkipped(
+                    "Possible SSL3_GET_SERVER_CERTIFICATE - If you run this test, make sure the certificate verification works"
+                );
 
                 return;
             } elseif ($errorCode >= 500) {
-                static::markTestSkipped("Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage());
+                static::markTestSkipped(
+                    "Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage()
+                );
             }
 
             static::assertTrue($assertThis, $errorMessage . " (" . $errorCode . ")");
@@ -201,19 +216,21 @@ class curlTest extends TestCase
      * @testdox Running "old style failing authentication mode" should generate blind errors like here
      * @throws \Exception
      */
-    function soapAuthErrorWithoutProtectiveFlag()
+    public function soapAuthErrorWithoutProtectiveFlag()
     {
         $this->CURL->setAuthentication("fail", "fail");
         $this->CURL->setFlag("NOSOAPWARNINGS");
         try {
             $this->CURL->doGet($this->wsdl);
         } catch (\Exception $e) {
-            // As of 6.0.16, SOAPWARNINGS are always enabled. Setting NOSOAPWARNINGS in flags, will render blind errors since the authentication errors are located in uncatchable warnings
+            // As of 6.0.16, SOAPWARNINGS are always enabled. Setting NOSOAPWARNINGS in flags,
+            // will render blind errors since the authentication errors are located in uncatchable warnings
             $errorCode = $e->getCode();
             if (version_compare(TORNELIB_NETCURL_RELEASE, "6.0.20", '>=')) {
                 static::assertTrue($errorCode == 500 ? true : false);
             } else {
-                // For older versions than 6.0.20, we can't turn SOAPWARNINGS off prorperly, so in those versions our errorcode 401 remains
+                // For older versions than 6.0.20, we can't turn SOAPWARNINGS off prorperly,
+                // so in those versions our errorcode 401 remains
                 static::assertTrue($errorCode == 401 ? true : false);
             }
         }
@@ -224,17 +241,19 @@ class curlTest extends TestCase
      * @testdox
      * @throws \Exception
      */
-    function soapAuthErrorNoInitialSoapFaultsNoWsdl()
+    public function soapAuthErrorNoInitialSoapFaultsNoWsdl()
     {
         $this->disableSslVerifyByPhpVersions();
         $this->CURL->setAuthentication("fail", "fail");
         try {
-            $this->CURL->doGet('https://test.resurs.com/ecommerce-test/ws/V4/SimplifiedShopFlowService',
-                NETCURL_POST_DATATYPES::DATATYPE_SOAP);
+            $this->CURL->doGet(
+                'https://test.resurs.com/ecommerce-test/ws/V4/SimplifiedShopFlowService',
+                NETCURL_POST_DATATYPES::DATATYPE_SOAP
+            );
         } catch (\Exception $e) {
             // As of 6.0.16, this is the default behaviour even when SOAPWARNINGS are not active by setFlag
             $errorMessage = $e->getMessage();
-            $errorCode    = $e->getCode();
+            $errorCode = $e->getCode();
 
             if (preg_match('/this when not in object context/i', $errorMessage)) {
                 static::markTestSkipped('This test might not support chaining: ' . $errorMessage);
@@ -247,25 +266,27 @@ class curlTest extends TestCase
                 $assertThis = true;
             }
             if (intval($errorCode) == 2) {
-                static::markTestSkipped("Possible SSL3_GET_SERVER_CERTIFICATE - If you run this test, make sure the certificate verification works");
+                static::markTestSkipped(
+                    "Possible SSL3_GET_SERVER_CERTIFICATE - If you run this test, make sure the certificate verification works"
+                );
 
                 return;
             } elseif ($errorCode >= 500) {
-                static::markTestSkipped("Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage());
+                static::markTestSkipped(
+                    "Got errors (" . $e->getCode() . ") on URL call, can't complete request: " . $e->getMessage()
+                );
             }
 
             static::assertTrue($assertThis, $errorMessage . " (" . $errorCode . ")");
         }
     }
 
-    // Deprecated: rbSoapBackToNoChain
-
     /**
      * @test
      * @testdox Test invalid function
      * @throws \Exception
      */
-    function rbFailSoapChain()
+    public function rbFailSoapChain()
     {
         $this->CURL->setFlag("SOAPCHAIN");
         $this->CURL->setAuthentication($this->username, $this->password);
@@ -277,12 +298,14 @@ class curlTest extends TestCase
         }
     }
 
+    // Deprecated: rbSoapBackToNoChain
+
     /**
      * @test
      * @throws \Exception
      * @since 6.0.20
      */
-    function rbSoapChain()
+    public function rbSoapChain()
     {
         $this->disableSslVerifyByPhpVersions();
         $this->CURL->setFlag("SOAPCHAIN");
@@ -296,19 +319,29 @@ class curlTest extends TestCase
         }
     }
 
-    // Currently dysfunctional
-    function rbSimpleXml()
+    public function rbSimpleXml()
     {
         try {
             $this->CURL->setAuthentication($this->username, $this->password);
             $this->CURL->setFlag('XMLSOAP', true);
             /** @noinspection PhpUndefinedMethodInspection */
             /** @var MODULE_CURL $wsdlResponse */
-            $wsdlResponse = $this->CURL->doGet($this->wsdl,
-                NETCURL_POST_DATATYPES::DATATYPE_SOAP_XML)->getPaymentMethods();
+            $wsdlResponse = $this->CURL->doGet(
+                $this->wsdl,
+                NETCURL_POST_DATATYPES::DATATYPE_SOAP_XML
+            )->getPaymentMethods();
         } catch (\Exception $e) {
             static::fail($e->getMessage());
         }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function setUp()
+    {
+        error_reporting(E_ALL);
+        $this->CURL = new MODULE_CURL();
     }
 
 }
