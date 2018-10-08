8bb90fbf001a6b7007fb3ffeaec7dd0f199434cd
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-10-08 10:33:19 +0200

    date

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -58,7 +58,7 @@ if (!defined('ECOMPHP_VERSION')) {
     define('ECOMPHP_VERSION', '1.3.13');
 }
 if (!defined('ECOMPHP_MODIFY_DATE')) {
-    define('ECOMPHP_MODIFY_DATE', '20181004');
+    define('ECOMPHP_MODIFY_DATE', '20181008');
 }
 
 /**
