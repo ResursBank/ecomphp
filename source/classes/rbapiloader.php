30a809ef9acedaeaaa58daff92e00e1e6feb6ceb
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-11-29 14:44:06 +0100

    1.1.41

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -58,7 +58,7 @@ if (!defined('ECOMPHP_VERSION')) {
     define('ECOMPHP_VERSION', '1.1.41');
 }
 if (!defined('ECOMPHP_MODIFY_DATE')) {
-    define('ECOMPHP_MODIFY_DATE', '20181128');
+    define('ECOMPHP_MODIFY_DATE', '20181129');
 }
 
 /**
