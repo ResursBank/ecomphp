635694bff399e0086fb2ef7da56975c4be1b04c3
Tomas Tornevall <thorne@tornevall.net>
2019-02-14 18:30:48 +0100

    Webdriver patch.

--- tests/webdriver.php
+++ tests/webdriver.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
