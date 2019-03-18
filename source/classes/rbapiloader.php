093ac933933e5792e34921056e0bfac3240a2d02
Tomas Tornevall <thorne@tornevall.net>
2019-03-18 10:54:12 +0100

    1.0.43: bitmask patch

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -7,7 +7,7 @@
  * @package RBEcomPHP
  * @author Resurs Bank Ecommerce <ecommerce.support@resurs.se>
  * @branch 1.0
- * @version 1.0.42
+ * @version 1.0.43
  * @deprecated Maintenance version only - Use composer based package v1.3 or higher if possible
  * @link https://test.resurs.com/docs/x/BACt Migration from 1.0/1.1 to 1.3 documentation
  * @link https://test.resurs.com/docs/x/TYNM Get started with EComPHP
@@ -53,10 +53,10 @@ use Resursbank\RBEcomPHP\RESURS_DEPRECATED_FLOW;
 
 // Globals starts here
 if (!defined('ECOMPHP_VERSION')) {
-    define('ECOMPHP_VERSION', '1.0.42');
+    define('ECOMPHP_VERSION', '1.0.43');
 }
 if (!defined('ECOMPHP_MODIFY_DATE')) {
-    define('ECOMPHP_MODIFY_DATE', '20190314');
+    define('ECOMPHP_MODIFY_DATE', '20190315');
 }
 
 /**
--- source/classes/rbapiloader/ResursTypeClasses.php
+++ source/classes/rbapiloader/ResursTypeClasses.php
@@ -198,7 +198,10 @@ abstract class RESURS_CALLBACK_REACHABILITY
  */
 abstract class RESURS_PAYMENT_STATUS_RETURNCODES
 {
-    const PAYMENT_STATUS_COULD_NOT_BE_SET = 0;  // No flags are set
+    /**
+     * Skip "not in use" since this off-value may cause flaws in status updates (true when matching false flags).
+     */
+    const NOT_IN_USE = 0;
     const PAYMENT_PENDING = 1;
     const PAYMENT_PROCESSING = 2;
     const PAYMENT_COMPLETED = 4;
@@ -206,6 +209,7 @@ abstract class RESURS_PAYMENT_STATUS_RETURNCODES
     const PAYMENT_CREDITED = 16;
     const PAYMENT_AUTOMATICALLY_DEBITED = 32;
     const PAYMENT_MANUAL_INSPECTION = 64;   // When an order by some reason gets stuck in manual inspections
+    const PAYMENT_STATUS_COULD_NOT_BE_SET = 128;  // No flags are set
 
     /** @deprecated Fallback status only, use PAYMENT_ANNULLED */
     const PAYMENT_CANCELLED = 8;
