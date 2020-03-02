ea372a85c1f14572cd96dc8e85050aa77fbffec5
Tomas Tornevall <tomas.tornevall@resurs.se>
2020-03-02 08:44:38 +0100

    Autotest problem fix. Behaviour changed with last commit. iframe-request must also be checked.

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -7,7 +7,7 @@
  * @author  Resurs Bank <support@resurs.se>
  * @author  Tomas Tornevall <tomas.tornevall@resurs.se>
  * @branch 1.1
- * @version 1.1.53
+ * @version 1.1.54
  * @deprecated Maintenance version only - Use composer based package v1.3 or higher if possible
  * @link https://test.resurs.com/docs/x/BACt Migration from 1.0/1.1 to 1.3 documentation
  * @link https://test.resurs.com/docs/x/TYNM Get started with EComPHP
@@ -56,10 +56,10 @@ use \Exception;
 
 // Globals starts here
 if (!defined('ECOMPHP_VERSION')) {
-    define('ECOMPHP_VERSION', '1.1.53');
+    define('ECOMPHP_VERSION', '1.1.54');
 }
 if (!defined('ECOMPHP_MODIFY_DATE')) {
-    define('ECOMPHP_MODIFY_DATE', '20200220');
+    define('ECOMPHP_MODIFY_DATE', '20200302');
 }
 
 /**
@@ -4198,7 +4198,9 @@ class ResursBank
                     }
 
                 } else {
-                    $return = $infoObject['block'];
+                    if ($iframe) {
+                        $return = $infoObject['block'];
+                    }
                 }
             }
         }
