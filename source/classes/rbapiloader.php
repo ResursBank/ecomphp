cc75aae5a23c0ed5af8089a1bafc3dd174747021
Tomas Tornevall <thorne@tornevall.net>
2020-05-14 11:10:24 +0200

    Fixing imports.

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -45,10 +45,7 @@ if (file_exists(RB_API_PATH . '/ecomhooks.php')) {
     require_once(RB_API_PATH . '/ecomhooks.php');
 }
 
-use Resursbank\RBEcomPHP\NETCURL_POST_DATATYPES;
-use Resursbank\RBEcomPHP\MODULE_CURL;
-use Resursbank\RBEcomPHP\MODULE_NETWORK;
-use Resursbank\RBEcomPHP\MODULE_CRYPTO;
+use Resursbank\RBEcomPHP\Bit;
 use Resursbank\RBEcomPHP\RESURS_AFTERSHOP_RENDER_TYPES;
 use Resursbank\RBEcomPHP\RESURS_CALLBACK_REACHABILITY;
 use Resursbank\RBEcomPHP\RESURS_CALLBACK_TYPES;
@@ -58,7 +55,14 @@ use Resursbank\RBEcomPHP\RESURS_ENVIRONMENTS;
 use Resursbank\RBEcomPHP\RESURS_FLOW_TYPES;
 use Resursbank\RBEcomPHP\RESURS_PAYMENT_STATUS_RETURNCODES;
 use Resursbank\RBEcomPHP\RESURS_URL_ENCODE_TYPES;
-use TorneLIB\Module\Bit;
+use Exception;
+use Resursbank\RBEcomPHP\Resursbank_Obsolete_Functions;
+use ResursException;
+use Resursbank\RBEcomPHP\MODULE_NETBITS;
+use Resursbank\RBEcomPHP\MODULE_CRYPTO;
+use Resursbank\RBEcomPHP\MODULE_CURL;
+use Resursbank\RBEcomPHP\MODULE_NETWORK;
+use Resursbank\RBEcomPHP\NETCURL_POST_DATATYPES;
 
 // Globals starts here. But should be deprecated if version tag can be fetched through their docblocks.
 if (!defined('ECOMPHP_VERSION')) {
@@ -862,7 +866,7 @@ class ResursBank
 
     /**
      * @param $enable
-     * @return $this
+     * @return \Resursbank\RBEcomPHP\ResursBank
      * @throws Exception
      */
     public function setWsdlCache($enable)
@@ -1124,7 +1128,7 @@ class ResursBank
                 $this->CURL->setWsdlCache($this->CURLDRIVER_WSDL_CACHE);
             }
 
-            $this->CURL->setChain(false);
+            //$this->CURL->setChain(false);
             if ($this->getSslSecurityDisabled()) {
                 $this->CURL->setSslVerify(false, false);
             }
@@ -2069,7 +2073,22 @@ class ResursBank
                 $ResursResponse = $this->CURL->getParsed();
             }
         } catch (\Exception $restException) {
-            throw new ResursException($restException->getMessage(), $restException->getCode());
+            $message = $restException->getMessage();
+            $code = $restException->getCode();
+            // Special recipes extracted from netcurl-6.1
+            if (method_exists($restException, 'getExtendException')) {
+                $extendedClass = $restException->getExtendException();
+                if (is_object($extendedClass) && method_exists($extendedClass, 'getParsed')) {
+                    $parsedExtended = $extendedClass->getParsed();
+                    if (isset($parsedExtended->description)) {
+                        $message .= ' (' . $parsedExtended->description . ')';
+                    }
+                    if (isset($parsedExtended->code) && $parsedExtended->code > 0) {
+                        $code = $parsedExtended->code;
+                    }
+                }
+            }
+            throw new ResursException($message, $code, $restException);
         }
         if ($ReturnAsArray) {
             $ResursResponseArray = [];
