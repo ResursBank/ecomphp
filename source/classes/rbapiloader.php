e530b16699ae900f4dda8b051f4f76603e75bafe
Tomas Tornevall <thorne@tornevall.net>
2022-01-26 19:40:07 +0100

    Version tagged to 1.3.69.

--- source/classes/rbapiloader.php
+++ source/classes/rbapiloader.php
@@ -74,7 +74,7 @@ if (!defined('ECOMPHP_VERSION')) {
     define('ECOMPHP_VERSION', (new Generic())->getVersionByAny(__FILE__, 3, ResursBank::class));
 }
 if (!defined('ECOMPHP_MODIFY_DATE')) {
-    define('ECOMPHP_MODIFY_DATE', '20220118');
+    define('ECOMPHP_MODIFY_DATE', '20220126');
 }
 
 /**
@@ -85,7 +85,7 @@ if (!defined('ECOMPHP_MODIFY_DATE')) {
 /**
  * Class ResursBank
  * @package Resursbank\RBEcomPHP
- * @version 1.3.68
+ * @version 1.3.69
  */
 class ResursBank
 {
